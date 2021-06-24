423814dac7345d1af5fc220be7f5e925b339eaaf
shuriavi <shuriavi@byui.edu>
2021-06-23 18:33:52 -0600

    preston

--- weather/js/preston.js
+++ weather/js/preston.js
@@ -1,16 +1,23 @@
 function toggleMenu() {
   document.getElementsByClassName("nav_bar")[0].classList.toggle("responsive");
-}
+ }
 
 const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var d = new Date();
-var day = d.getDay() 
+var day = d.getDay(); 
 var date = d.getDate();
 var month = d.getMonth();
 var year = d.getFullYear();
-
-var dateStr = day1[day] + ', ' + date + " " + month1[month] + " " + year;
+var dateStr = day1[day] + ', ' + date + ' ' + month1[month] + ' ' + year;
 document.getElementById("dates").innerHTML = dateStr;
+document.getElementById("copyright").innerHTML = year;
 
-document.getElementById("copyright").innerHTML = year
+
+if (day1[day] == 'Friday') {
+  document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+  document.getElementById("announce").style.display = "block";
+}
+else {
+  document.getElementById("announce").style.display = "none";
+}
