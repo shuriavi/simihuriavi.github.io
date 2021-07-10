8a9fa480b2313aac61d555506eefa0a7d3a49338
shuriavi <shuriavi@byui.edu>
2021-07-10 14:27:21 -0600

    preston 10

---
+++ week5/preston-10/js/script.js
@@ -0,0 +1,64 @@
+//Hamburger icon
+function toggleMenu() {
+    document.getElementsByClassName("nav_bar")[0].classList.toggle("responsive");
+   }
+
+//Get Footer Date
+const day1 = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const month1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var d = new Date();
+var day = d.getDay(); 
+var date = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+var dateStr = day1[day] + ', ' + date + ' ' + month1[month] + ' ' + year;
+document.getElementById("dates").innerHTML = dateStr;
+document.getElementById("copyright").innerHTML = year;
+
+//Friday Announcement
+if (day1[day] == 'Friday') {
+    document.getElementById("announce").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.getElementById("announce").style.display = "block";
+}
+else {
+    document.getElementById("announce").style.display = "none";
+}
+
+// Current Weather for Weather Summary
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((town) => {
+    console.log(town);
+    let description = town.weather[0].description;
+    document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+    document.getElementById('temp').innerHTML = Math.round(town.main.temp);
+    document.getElementById('humidity').innerHTML = town.main.humidity;
+    document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
+});
+
+// 5 day forecast
+
+const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=da923bb8ec61575a7dfe5e3106bb43c1"
+
+fetch(apiURL_forecast)
+    .then(response => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
+
+	console.log(forecastData);
+
+	const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+    let day = 0;
+	forecastData.forEach(forecast => {
+	  let x = new Date(forecast.dt_txt);
+    document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
+    document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+    document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
+	document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
+	day++;	  
+	});
+});
